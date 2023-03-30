3f9710f21fad09db5bf0ac2cae346c1c095cd12c
wrp801 <wespoulsen@gmail.com>
2023-03-30 14:55:31 -0600

    Added regex match functionality

--- domo_blueprints/upload_csv_to_dataset.py
+++ domo_blueprints/upload_csv_to_dataset.py
@@ -7,10 +7,12 @@ import shipyard_utils as shipyard
 import pandas as pd
 import os
 import ast
+import re
+import typing
 from random import random, randrange
 from itertools import islice
 from io import StringIO
-from math import exp, log, floor
+from math import exp, log, floor, ceil
 try:
     import errors as ec
 except BaseException:
@@ -29,6 +31,7 @@ def get_args():
     parser.add_argument("--domo-schema", dest = 'domo_schema',required = False, default = '')
     parser.add_argument("--insert-method", dest = 'insert_method' ,default = 'REPLACE',choices={"REPLACE","APPEND"},required=True)
     parser.add_argument("--dataset-id", required=False, default='',dest='dataset_id')
+    parser.add_argument("--source-file-match-type", dest = "source_file_match_type", choices= {'regex_match', 'exact_match'}, default = 'exact_match', required = False)
     args = parser.parse_args()
 
     return args
@@ -88,15 +91,33 @@ def infer_schema(file_name:str, folder_name, domo_instance:Domo, k=10000):
     Returns:
         Schema: Schema object of the dataset
     """
-    file_path = file_name
-    if folder_name is not None:
-        file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file_name))
-    with open(file_path, 'r') as f:
-        header = next(f)
-        result = [header] + reservoir_sample(f, k)
-    df = pd.read_csv(StringIO(''.join(result)))
-    schema = domo_instance.utilities.data_schema(df)
-    return Schema(schema)
+    if isinstance(file_name, list):
+        dataframes = []
+        n_files = len(file_name)
+        rows_per_file = ceil(k/n_files)
+        for file in file_name:
+            file_path = file
+            if folder_name is not None:
+                file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file)) 
+            with open(file_path, 'r') as f:
+                header = next(f)
+                result  = [header] + reservoir_sample(f, rows_per_file)
+            df = pd.read_csv(StringIO(''.join(result)))
+            dataframes.append(df)
+        merged = pd.concat(dataframes, axis = 0, ignore_index=True)
+        schema = domo_instance.utilities.data_schema(merged)
+        return Schema(schema)
+
+    else:
+        file_path = file_name
+        if folder_name is not None:
+            file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file_name))
+        with open(file_path, 'r') as f:
+            header = next(f)
+            result = [header] + reservoir_sample(f, k)
+        df = pd.read_csv(StringIO(''.join(result)))
+        schema = domo_instance.utilities.data_schema(df)
+        return Schema(schema)
 
 
 def make_schema(data_types:list, file_name:str, folder_name:str):
@@ -110,26 +131,52 @@ def make_schema(data_types:list, file_name:str, folder_name:str):
     Returns:
         Schema: Schema object of the dataset
     """
-    file_path = file_name
-    if folder_name is not None:
-        file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file_name))
-    df = pd.read_csv(file_path,nrows=1)
-    cols = list(df.columns)
-    if len(cols) != len(data_types):
-        print("Error: The number data types does not equal the number of columns. Please number of domo data types provided matches the number of columns")
-        sys.exit(ec.EXIT_CODE_COLUMN_MISMATCH)
+    if isinstance(file_name, list):
+        schemas = []
+        for file in file_name:
+            file_path = file
+            if folder_name is not None:
+                file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file_name))
+            df = pd.read_csv(file_path,nrows=1)
+            cols = list(df.columns)
+            if len(cols) != len(data_types):
+                print("Error: The number data types does not equal the number of columns. Please number of domo data types provided matches the number of columns")
+                sys.exit(ec.EXIT_CODE_COLUMN_MISMATCH)
+            domo_schema = []
+            for pair in data_types:
+                col = pair[0]
+                dtype = pair[1]
+                dt_upper = str(dtype).upper()
+                if dt_upper not in ['STRING','DECIMAL','LONG','DOUBLE','DATE','DATETIME']:
+                    print(f"Error: {dt_upper} is not a valid domo data type. Please ensure one of STRING, DECIMAL, LONG, DOUBLE, DATE, DATETIME is selected")
+                    sys.exit(ec.EXIT_CODE_INVALID_DATA_TYPE)
+                domo_schema.append(Column(dt_upper,col))
+                schema = Schema(domo_schema)
+                schemas.append(schema)
+        assert all(schemas[0] == s for s in schemas)
+        return schemas[0]
+    else:
 
-    domo_schema = []
-    for pair in data_types:
-        col = pair[0]
-        dtype = pair[1]
-        dt_upper = str(dtype).upper()
-        if dt_upper not in ['STRING','DECIMAL','LONG','DOUBLE','DATE','DATETIME']:
-            print(f"Error: {dt_upper} is not a valid domo data type. Please ensure one of STRING, DECIMAL, LONG, DOUBLE, DATE, DATETIME is selected")
-            sys.exit(ec.EXIT_CODE_INVALID_DATA_TYPE)
-        domo_schema.append(Column(dt_upper,col))
-    
-    return Schema(domo_schema)
+        file_path = file_name
+        if folder_name is not None:
+            file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file_name))
+        df = pd.read_csv(file_path,nrows=1)
+        cols = list(df.columns)
+        if len(cols) != len(data_types):
+            print("Error: The number data types does not equal the number of columns. Please number of domo data types provided matches the number of columns")
+            sys.exit(ec.EXIT_CODE_COLUMN_MISMATCH)
+
+        domo_schema = []
+        for pair in data_types:
+            col = pair[0]
+            dtype = pair[1]
+            dt_upper = str(dtype).upper()
+            if dt_upper not in ['STRING','DECIMAL','LONG','DOUBLE','DATE','DATETIME']:
+                print(f"Error: {dt_upper} is not a valid domo data type. Please ensure one of STRING, DECIMAL, LONG, DOUBLE, DATE, DATETIME is selected")
+                sys.exit(ec.EXIT_CODE_INVALID_DATA_TYPE)
+            domo_schema.append(Column(dt_upper,col))
+        
+        return Schema(domo_schema)
 
 
 def dataset_exists(datasets, dataset_name):
@@ -140,7 +187,7 @@ def upload_stream(domo_instance:Domo, file_name:str, dataset_name:str, update_me
 
     Args:
         domo_instance (Domo): connection to Domo
-        file_name (str): The file path of the dataset
+        file_name (str | list): The file path of the dataset. If Regex match is selected, then this will be a list
         dataset_name (str): The name of the dataset
         update_method (str): The update method (REPLACE or APPEND)
         dataset_id (str): The id of the dataset if modifying an existing one
@@ -149,22 +196,12 @@ def upload_stream(domo_instance:Domo, file_name:str, dataset_name:str, update_me
         domo_schema (_type_, optional): Optional schema of the dataset. If omitted, then the data types will be inferred using sampling
     """
     file_path = file_name
-    if folder_name is not None:
-        file_path = os.path.normpath(os.path.join(os.getcwd(),folder_name,file_name))
-    # Check to see if the dataset name exists and the id is not provided. If it exists, then exit
-    if dataset_id == '':
-        all_datasets = domo_instance.ds_list()
-        if dataset_exists(all_datasets,dataset_name):
-            print(f"Error: A dataset with the name {dataset_name} already exists. Please provide a unique name to create a new dataset or provide the dataset id to update an existing one")
-            sys.exit(ec.EXIT_CODE_DUPLICATE_DATASET)
-
     streams = domo_instance.streams
     dsr = DataSetRequest()
     dsr.name = dataset_name
     dsr.schema = domo_schema
     if dataset_description is not None:
         dsr.description = dataset_description
-
 
     if dataset_id != '': # if a dataset id has been provided, meaning an existing dataset will be modified
         # check to see if the schemas are identical
@@ -191,14 +228,24 @@ def upload_stream(domo_instance:Domo, file_name:str, dataset_name:str, update_me
     execution = streams.create_execution(stream_id)
     execution_id = execution['id']
 
-    # Load the data into domo by chunks and parts
-    for part, chunk in enumerate(pd.read_csv(file_path,chunksize=CHUNKSIZE,dtype=pandas_dtypes),start = 1):
-        execution = streams.upload_part(stream_id, execution_id,part,chunk.to_csv(index=False))
-    
+    # if the regex match is selected, load all the files to a single domo dataset
+    if isinstance(file_name, list):
+        index = 0
+        for file_num, file in enumerate(file_name,start = 0):
+            for part, chunk in enumerate(pd.read_csv(file, chunksize= CHUNKSIZE, dtype = pandas_dtypes),start = 1):
+                index += 1
+                execution = streams.upload_part(stream_id, execution_id, index, chunk.to_csv(index = False))
+                
+    # otherwise load a single file
+    else:
+        # Load the data into domo by chunks and parts
+        for part, chunk in enumerate(pd.read_csv(file_path,chunksize=CHUNKSIZE,dtype=pandas_dtypes),start = 1):
+            execution = streams.upload_part(stream_id, execution_id,part,chunk.to_csv(index=False))
+
+    # commit the stream 
     commited_execution = streams.commit_execution(stream_id,execution_id)
     print("Successfully loaded dataset to domo")
     return stream_id, execution_id
-
 
 def main():
     args = get_args()
@@ -210,6 +257,7 @@ def main():
     folder_name = args.folder_name
     insert_method = args.insert_method
     dataset_id = args.dataset_id
+    match_type = args.source_file_match_type
     if args.domo_schema != '':
         domo_schema = args.domo_schema
         domo_schema = ast.literal_eval(domo_schema)
@@ -226,21 +274,49 @@ def main():
         print(e)
         sys.exit(ec.EXIT_CODE_INVALID_CREDENTIALS)
     # if the schema is provided, then use that otherwise infer the schema using sampling
-    if args.domo_schema != '':
-        dataset_schema = make_schema(domo_schema, file_to_load, folder_name)
-    else:
-        dataset_schema = infer_schema(file_to_load, folder_name, domo, k = 10000)
+    # if args.domo_schema != '':
+    #     dataset_schema = make_schema(domo_schema, file_to_load, folder_name)
+    # else:
+    #     dataset_schema = infer_schema(file_to_load, folder_name, domo, k = 10000)
     
-    stream_id, execution_id = upload_stream(domo, file_to_load, dataset_name,insert_method, dataset_id, 
-        folder_name, dataset_description, dataset_schema)
+    if match_type == 'regex_match':
+        file_names = shipyard.files.find_all_local_file_names(
+        folder_name)
+        matching_file_names = shipyard.files.find_all_file_matches(
+        file_names, re.compile(file_to_load))
+        print(f'{len(matching_file_names)} files found. Preparing to upload...')
+        # if the schema is provided, then use that otherwise infer the schema using sampling
+        if args.domo_schema != '':
+            dataset_schema = make_schema(domo_schema, matching_file_names, folder_name)
+        else:
+            dataset_schema = infer_schema(matching_file_names, folder_name, domo, k = 10000)
+        stream_id, execution_id = upload_stream(domo, matching_file_names, dataset_name,
+                                                insert_method, dataset_id, 
+                                                folder_name, dataset_description, dataset_schema)
+        base_folder_name = shipyard.logs.determine_base_artifact_folder(
+            'domo')
+        artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+            base_folder_name)
+        shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
+        shipyard.logs.create_pickle_file(artifact_subfolder_paths, 'stream_id', stream_id)
+        shipyard.logs.create_pickle_file(artifact_subfolder_paths, 'execution_id', execution_id)
 
-    base_folder_name = shipyard.logs.determine_base_artifact_folder(
-        'domo')
-    artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
-        base_folder_name)
-    shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
-    shipyard.logs.create_pickle_file(artifact_subfolder_paths, 'stream_id', stream_id)
-    shipyard.logs.create_pickle_file(artifact_subfolder_paths, 'execution_id', execution_id)
+    else:
+        # if the schema is provided, then use that otherwise infer the schema using sampling
+        if args.domo_schema != '':
+            dataset_schema = make_schema(domo_schema, file_to_load, folder_name)
+        else:
+            dataset_schema = infer_schema(file_to_load, folder_name, domo, k = 10000)
+        stream_id, execution_id = upload_stream(domo, file_to_load, dataset_name,insert_method, dataset_id, 
+            folder_name, dataset_description, dataset_schema)
+
+        base_folder_name = shipyard.logs.determine_base_artifact_folder(
+            'domo')
+        artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+            base_folder_name)
+        shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
+        shipyard.logs.create_pickle_file(artifact_subfolder_paths, 'stream_id', stream_id)
+        shipyard.logs.create_pickle_file(artifact_subfolder_paths, 'execution_id', execution_id)
 
 if __name__ == "__main__":
     main()
